4086449b1830ad4623be018319c9d98e354f1dfb
DiegoMiranda-dev <mirandadiego864@gmail.com>
2024-07-16 23:00:59 -0600

    divisa class

---
+++ src/Divisa.java
@@ -0,0 +1,13 @@
+
+public class Divisa {
+    private double conversion_result;
+
+    public double getConversion_result() {
+        return conversion_result;
+    }
+
+    public void setConversion_result(double conversion_result) {
+        this.conversion_result = conversion_result;
+    }
+
+}
